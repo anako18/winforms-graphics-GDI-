047d27aedc1711d423934123c9020e616076ef0c
anako18 <nuka97@yandex.ru>
2018-01-09 20:52:55 +0300

    Done.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace GRAPHICS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.th_l = new System.Windows.Forms.Label();
+            this.op_l = new System.Windows.Forms.Label();
+            this.color_l = new System.Windows.Forms.Label();
+            this.trackBarTHval = new System.Windows.Forms.Label();
+            this.trackBarOPval = new System.Windows.Forms.Label();
+            this.trackBarTH = new System.Windows.Forms.TrackBar();
+            this.trackBarOP = new System.Windows.Forms.TrackBar();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarTH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menu.AutoSize = false;
+            this.menu.Dock = System.Windows.Forms.DockStyle.None;
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menu.Size = new System.Drawing.Size(713, 30);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.AutoSize = false;
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newMenu,
+            this.openMenu,
+            this.saveMenu,
+            this.exitMenu});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // newMenu
+            // 
+            this.newMenu.Name = "newMenu";
+            this.newMenu.Size = new System.Drawing.Size(129, 26);
+            this.newMenu.Text = "New";
+            this.newMenu.Click += new System.EventHandler(this.newMenu_Click);
+            // 
+            // openMenu
+            // 
+            this.openMenu.Name = "openMenu";
+            this.openMenu.Size = new System.Drawing.Size(129, 26);
+            this.openMenu.Text = "Open...";
+            this.openMenu.Click += new System.EventHandler(this.openMenu_Click);
+            // 
+            // saveMenu
+            // 
+            this.saveMenu.Name = "saveMenu";
+            this.saveMenu.Size = new System.Drawing.Size(129, 26);
+            this.saveMenu.Text = "Save...";
+            this.saveMenu.Click += new System.EventHandler(this.saveMenu_Click);
+            // 
+            // exitMenu
+            // 
+            this.exitMenu.Name = "exitMenu";
+            this.exitMenu.Size = new System.Drawing.Size(129, 26);
+            this.exitMenu.Text = "Exit";
+            this.exitMenu.Click += new System.EventHandler(this.exitMenu_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.th_l);
+            this.panel1.Controls.Add(this.op_l);
+            this.panel1.Controls.Add(this.color_l);
+            this.panel1.Controls.Add(this.trackBarTHval);
+            this.panel1.Controls.Add(this.trackBarOPval);
+            this.panel1.Controls.Add(this.trackBarTH);
+            this.panel1.Controls.Add(this.trackBarOP);
+            this.panel1.Location = new System.Drawing.Point(0, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(713, 64);
+            this.panel1.TabIndex = 1;
+            // 
+            // th_l
+            // 
+            this.th_l.AutoSize = true;
+            this.th_l.BackColor = System.Drawing.Color.White;
+            this.th_l.Location = new System.Drawing.Point(435, 43);
+            this.th_l.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.th_l.Name = "th_l";
+            this.th_l.Size = new System.Drawing.Size(72, 17);
+            this.th_l.TabIndex = 6;
+            this.th_l.Text = "Thickness";
+            // 
+            // op_l
+            // 
+            this.op_l.AutoSize = true;
+            this.op_l.BackColor = System.Drawing.Color.White;
+            this.op_l.Location = new System.Drawing.Point(103, 43);
+            this.op_l.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.op_l.Name = "op_l";
+            this.op_l.Size = new System.Drawing.Size(56, 17);
+            this.op_l.TabIndex = 5;
+            this.op_l.Text = "Opacity";
+            // 
+            // color_l
+            // 
+            this.color_l.BackColor = System.Drawing.Color.Black;
+            this.color_l.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.color_l.Location = new System.Drawing.Point(659, 14);
+            this.color_l.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.color_l.Name = "color_l";
+            this.color_l.Size = new System.Drawing.Size(33, 28);
+            this.color_l.TabIndex = 0;
+            this.color_l.Click += new System.EventHandler(this.color_l_Click);
+            // 
+            // trackBarTHval
+            // 
+            this.trackBarTHval.AutoSize = true;
+            this.trackBarTHval.Location = new System.Drawing.Point(597, 26);
+            this.trackBarTHval.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.trackBarTHval.Name = "trackBarTHval";
+            this.trackBarTHval.Size = new System.Drawing.Size(24, 17);
+            this.trackBarTHval.TabIndex = 4;
+            this.trackBarTHval.Text = "15";
+            // 
+            // trackBarOPval
+            // 
+            this.trackBarOPval.AutoSize = true;
+            this.trackBarOPval.Location = new System.Drawing.Point(265, 26);
+            this.trackBarOPval.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.trackBarOPval.Name = "trackBarOPval";
+            this.trackBarOPval.Size = new System.Drawing.Size(32, 17);
+            this.trackBarOPval.TabIndex = 3;
+            this.trackBarOPval.Text = "255";
+            // 
+            // trackBarTH
+            // 
+            this.trackBarTH.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.trackBarTH.Location = new System.Drawing.Point(347, 4);
+            this.trackBarTH.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBarTH.Maximum = 100;
+            this.trackBarTH.Name = "trackBarTH";
+            this.trackBarTH.Size = new System.Drawing.Size(243, 56);
+            this.trackBarTH.TabIndex = 2;
+            this.trackBarTH.TickFrequency = 10;
+            this.trackBarTH.Value = 50;
+            this.trackBarTH.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
+            // 
+            // trackBarOP
+            // 
+            this.trackBarOP.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.trackBarOP.LargeChange = 10;
+            this.trackBarOP.Location = new System.Drawing.Point(15, 4);
+            this.trackBarOP.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBarOP.Maximum = 255;
+            this.trackBarOP.Name = "trackBarOP";
+            this.trackBarOP.Size = new System.Drawing.Size(243, 56);
+            this.trackBarOP.TabIndex = 1;
+            this.trackBarOP.TickFrequency = 17;
+            this.trackBarOP.Value = 128;
+            this.trackBarOP.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox.Location = new System.Drawing.Point(0, 94);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(535, 217);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(709, 359);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarTH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newMenu;
+        private System.Windows.Forms.ToolStripMenuItem openMenu;
+        private System.Windows.Forms.ToolStripMenuItem saveMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitMenu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBarTH;
+        private System.Windows.Forms.TrackBar trackBarOP;
+        private System.Windows.Forms.Label color_l;
+        private System.Windows.Forms.Label th_l;
+        private System.Windows.Forms.Label op_l;
+        private System.Windows.Forms.Label trackBarTHval;
+        private System.Windows.Forms.Label trackBarOPval;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GRAPHICS
+{
+    public partial class Form1 : Form
+    {
+        bool draw = false;
+        bool image_exists = false;
+        bool is_saved = true;
+
+        string filepath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            TrackBar tb = sender as TrackBar;
+            int value = tb.Value;
+            string labelname = tb.Name + "val";
+            Label val = Controls["panel1"].Controls[labelname] as Label;
+            val.Text = value.ToString();
+        }
+
+        private Graphics img;
+
+        private void newMenu_Click(object sender, EventArgs e)
+        {
+            Bitmap im = new Bitmap(pictureBox.Width, pictureBox.Height);
+            //Graphics img = Graphics.FromImage(im);
+            img = Graphics.FromImage(im);
+            img.FillRectangle(Brushes.White, 0, 0, pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = im;
+            image_exists = true;
+            filepath = "";
+            this.Text = "NewImage.bmp*";
+        }
+
+        private void openMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open...";
+            ofd.Filter = "Bitmap(*.bmp) | *.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filepath = ofd.FileName;
+                this.Text = Path.GetFileName(filepath);
+                if (pictureBox.Image != null)
+                    pictureBox.Image.Dispose();
+                pictureBox.Image = new Bitmap(ofd.FileName);
+                image_exists = true;
+            }
+        }
+
+        private void saveMenu_Click(object sender, EventArgs e)
+        {
+            if (!is_saved)
+                Save();
+        }
+
+        private void Save()
+        {
+            //SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (filepath != "")
+            {
+                string message = "Сохранить изменения в файле " + filepath + "?";
+                DialogResult result = MessageBox.Show(message, "GRAPHICS", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    string temp = Path.GetDirectoryName(filepath) + "\\temp.bmp";
+                    pictureBox.Image.Save(temp);
+                    pictureBox.Image.Dispose();
+          
+                    File.Delete(filepath);
+                    File.Move(temp, filepath);
+                    File.Delete(temp);         
+                     pictureBox.Image = new Bitmap(filepath);
+                    this.Text = this.Text.Replace("*", "");
+                    is_saved = true;
+                }
+            }
+            else
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Save";
+                sfd.Filter = "Bitmap|*.bmp";
+                DialogResult result = sfd.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    filepath = sfd.FileName;
+                    pictureBox.Image.Save(filepath);
+                    this.Text = Path.GetFileName(filepath);
+                    is_saved = true;
+                }
+            }
+        }
+
+        private void exitMenu_Click(object sender, EventArgs e)
+        {
+           
+            Close();
+        }
+
+        private void color_l_Click(object sender, EventArgs e)
+        { 
+            Label l = sender as Label;
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                l.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private Color get_color()
+        {
+            return Color.FromArgb(trackBarOP.Value, color_l.BackColor.R, color_l.BackColor.G, color_l.BackColor.B);
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (image_exists)
+            {
+                draw = true;
+                Draw(e.X, e.Y);
+                is_saved = false;
+                if (!this.Text.Contains("*"))
+                this.Text += "*";
+            }
+        }
+
+        private void Draw(int ex, int ey)
+        {
+            Image im = pictureBox.Image;
+            Color c = get_color();
+            int thickness = trackBarTH.Value;
+            Graphics g = Graphics.FromImage(im);
+
+            Rectangle rect = new Rectangle(
+                ex,ey,
+                thickness,
+                thickness);
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(rect);
+
+            using (PathGradientBrush brush = new PathGradientBrush(path))
+            {
+                brush.CenterColor = c;
+                brush.SurroundColors = new Color[] { Color.Transparent }; ;
+                g.FillEllipse(brush, rect);
+            }
+            g.Save();
+            pictureBox.Image = im;
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                Draw(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!is_saved)
+                Save();
+        }
+
+    }
+}
